037ee2cff28198f2c7f839ebe3277a9cf8d922ad
YILING <yiling@candyhouse.co>
2016-07-17 13:42:19 +0800

    ignore

--- build/PatPet.build/Debug-iphoneos/PatPet.build/Objects-normal/arm64/NavigationViewController.d
+++ build/PatPet.build/Debug-iphoneos/PatPet.build/Objects-normal/arm64/NavigationViewController.d
@@ -3,4 +3,5 @@ dependencies: \
   /Users/YLibrarian/Documents/Project/KuPetProject/PetsPatting/petspatting/PatPet/NavigationViewController.h \
   /Users/YLibrarian/Documents/Project/KuPetProject/PetsPatting/petspatting/PatPet/SlideOutMenuFiles/SWRevealViewController.h \
   /Users/YLibrarian/Documents/Project/KuPetProject/PetsPatting/petspatting/PatPet/Model/PetColor.h \
-  /Users/YLibrarian/Documents/Project/KuPetProject/PetsPatting/petspatting/PatPet/UIColor/UIColor+Hex.h
+  /Users/YLibrarian/Documents/Project/KuPetProject/PetsPatting/petspatting/PatPet/UIColor/UIColor+Hex.h \
+  /Users/YLibrarian/Documents/Project/KuPetProject/PetsPatting/petspatting/PatPet/YLViews/YLCircle.h
